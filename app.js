ff1ea4093f4efc3e9848df7e76f6fd839d7677b1
潘星 <1377689310@qq.com>
2021-03-15 22:25:56 +0800

    eee

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    song: '', // 全局中正在播放的音乐。
+    mv: '', // 全局中正在播放的mv
   }
 })
--- packageSong/pages/songDetail/index.js
+++ packageSong/pages/songDetail/index.js
@@ -1,7 +1,8 @@
 import regeneratorRuntime from 'regenerator-runtime'
 const api = require('../../../router/api');
 const util = require('../../../utils/util')
-
+const app = getApp();
+const globalData = app.globalData
 Page({
   data: {
 
@@ -11,9 +12,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (e) {
+    console.log(globalData)
     let that = this
     this.setData({
-      songmid:e.id
+      songmid:e.id,
+      mid:e.mid
     },() => {
       that.getSongInfo();
       that.getLyric();
@@ -44,23 +47,56 @@ Page({
   onShareAppMessage: function () {
 
   },
+  play(){
+    const innerAudioContext = wx.createInnerAudioContext();
+    globalData.song = this.data.mid;
+    innerAudioContext.autoplay = true
+    innerAudioContext.src = `${this.data.playUrl}`
+    innerAudioContext.onPlay(() => {
+      console.log('开始播放')
+    })
+    innerAudioContext.onError((res) => {
+      console.log(res.errMsg)
+      console.log(res.errCode)
+    })
+  },
   async getSongInfo(){
     let temp = await util.request(`${api.getSongInfo}?songmid=${this.data.songmid}`,{},"get");
-    console.log('getSongInfo',temp.data.response.songinfo)
+    console.log('getSongInfo',temp.data.response.songinfo);
+    this.setData({
+      songinfo:temp.data.response.songinfo
+    });
   },
   async getMusicPlay(){
     let temp = await util.request(`${api.getMusicPlay}?songmid=${this.data.songmid}&justPlayUrl=all`,{},"get");
-    console.log('getMusicPlay',temp.data.data.playUrl)
+    console.log('getMusicPlay',temp.data.data.playUrl);
+    this.setData({
+      playUrl:temp.data.data.playUrl[`${this.data.songmid}`].url
+    },() => {
+      if(this.data.playUrl){
+        this.play();
+      } else {
+        util.pxshowErrorToast('抱歉，暂无该歌曲资源',1000);
+      }
+    })
   },
   async getLyric(){
     let temp = await util.request(`${api.getLyric}?songmid=${this.data.songmid}`,{},"get");
-    console.log('getLyric',temp.data.response.lyric)
+    console.log('getLyric',temp.data.response.lyric);
+    let songlyric = temp.data.response.lyric.replace(new RegExp(/\[(\S*)\]/g),'');
+    let songTime = temp.data.response.lyric.match(new RegExp(/\[(\S*)\]/g));
+    songTime.splice(0,4)
+    this.setData({
+      lyric:songlyric,
+      songTime:songTime
+    })
   },
   async getImageUrl(){
-    let temp = await util.request(`${api.getImageUrl}?id=${this.data.songmid}`,{},"get");
+    let temp = await util.request(`${api.getImageUrl}?id=${this.data.mid}`,{},"get");
     console.log('getImageUrl',temp.data.response.data.imageUrl);
     this.setData({
       imageUrl:temp.data.response.data.imageUrl
     })
-  }
+  },
+  
 })
