94e4aea96f8d23fae6878bdcd7bd32e1cc2e089d
潘星 <1377689310@qq.com>
2021-03-17 18:25:21 +0800

    ee

--- components/musictabs/index.js
+++ components/musictabs/index.js
@@ -1,23 +1,67 @@
-// components/musictabs/index.js
+import regeneratorRuntime from 'regenerator-runtime'
+const util = require('../../../utils/util');
+const api = require('../../../router/api');
+const music = require('../../../utils/musicplay');
+
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-
+    songmid:{
+      type:String,
+      value:''
+    },
+    albummid:{
+      type:String,
+      value:''
+    },
+    show:{
+      type:Boolean,
+      value: false
+    },
+    songinfo:{
+      type:Object,
+      value:{}
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    playUrl:'',
+    imageUrl:''
   },
-
+  lifetimes:{
+    ready(){
+      this.getMusicPlay();
+      this.getImageUrl();
+    }
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
+    async getMusicPlay(){
+      let temp = await util.request(`${api.getMusicPlay}?songmid=${this.data.songmid}&justPlayUrl=all`,{},"get");
+      this.setData({
+        playUrl:temp.data.data.playUrl[`${this.data.songmid}`].url
+      },() => {
+        if(!this.data.playUrl){
+          util.pxshowErrorToast('抱歉，暂无该歌曲资源',1000);
+          return ;
+        }
+        music.init(this.data.playUrl);
+        music.play();
+      })
+    },
+    async getImageUrl(){
+      let temp = await util.request(`${api.getImageUrl}?id=${this.data.albummid}`,{},"get");
+      console.log('getImageUrl',temp.data.response.data.imageUrl);
+      this.setData({
+        imageUrl:temp.data.response.data.imageUrl
+      })
+    }
   }
-})
+})  
--- packageSong/pages/songDetailList/index.js
+++ packageSong/pages/songDetailList/index.js
@@ -1,7 +1,7 @@
 import regeneratorRuntime from 'regenerator-runtime'
 const util = require('../../../utils/util');
 const api = require('../../../router/api');
-const music = require('../../../utils/musicplay');
+
 Page({
 
   data: {
@@ -40,19 +40,31 @@ Page({
   },
   playMusic(e){
     let temp = e.currentTarget.dataset.item;
-    this.getMusicPlay(temp.mid);
-  },
-  async getMusicPlay(songmid){
-    let temp = await util.request(`${api.getMusicPlay}?songmid=${songmid}&justPlayUrl=all`,{},"get");
     this.setData({
-      playUrl:temp.data.data.playUrl[`${songmid}`].url
+      songmid: temp.mid,
+      albummid: temp.album.mid,
+      songinfo: temp,
+      showMusicbar: true
+    })
+  },
+  async getMusicPlay(){
+    let temp = await util.request(`${api.getMusicPlay}?songmid=${this.data.songmid}&justPlayUrl=all`,{},"get");
+    this.setData({
+      playUrl:temp.data.data.playUrl[`${this.data.songmid}`].url
     },() => {
       if(!this.data.playUrl){
         util.pxshowErrorToast('抱歉，暂无该歌曲资源',1000);
         return ;
       }
-      music.init(this.data.playUrl);
-      music.play();
+      // music.init(this.data.playUrl);
+      // music.play();
+    })
+  },
+  async getImageUrl(){
+    let temp = await util.request(`${api.getImageUrl}?id=${this.data.albummid}`,{},"get");
+    console.log('getImageUrl',temp.data.response.data.imageUrl);
+    this.setData({
+      imageUrl:temp.data.response.data.imageUrl
     })
   },
 })
